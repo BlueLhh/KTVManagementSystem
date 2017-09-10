6ffd5ca571b48a69fd17a93e14886027b51b9fd7
BlueLhh <lanhonghui100@gmail.com>
2017-09-10 13:57:02 +0800

    完成商品实体类

--- src/com/lhh/ktv/model/entity/Goods.java
+++ src/com/lhh/ktv/model/entity/Goods.java
@@ -9,5 +9,59 @@ package com.lhh.ktv.model.entity;
  *
  */
 public class Goods {
+	private Long goodsId;//商品id
+	private String goodsName;//商品名称
+	private double goodsPrice;//商品价格
+	private int goodsCount;//商品库存
+	
+	public Goods() {
+		
+	}
+	
+	public Goods(Long goodsId, String goodsName, double goodsPrice, int goodsCount) {
+		super();
+		this.goodsId = goodsId;
+		this.goodsName = goodsName;
+		this.goodsPrice = goodsPrice;
+		this.goodsCount = goodsCount;
+	}
 
+	public Long getGoodsId() {
+		return goodsId;
+	}
+
+	public void setGoodsId(Long goodsId) {
+		this.goodsId = goodsId;
+	}
+
+	public String getGoodsName() {
+		return goodsName;
+	}
+
+	public void setGoodsName(String goodsName) {
+		this.goodsName = goodsName;
+	}
+
+	public double getGoodsPrice() {
+		return goodsPrice;
+	}
+
+	public void setGoodsPrice(double goodsPrice) {
+		this.goodsPrice = goodsPrice;
+	}
+
+	public int getGoodsCount() {
+		return goodsCount;
+	}
+
+	public void setGoodsCount(int goodsCount) {
+		this.goodsCount = goodsCount;
+	}
+
+	@Override
+	public String toString() {
+		return "商品信息 [商品编号=" + goodsId + ", 商品名称=" + goodsName + ", 商品价格=" + goodsPrice + ", 商品库存="
+				+ goodsCount + "]";
+	}
+	
 }
