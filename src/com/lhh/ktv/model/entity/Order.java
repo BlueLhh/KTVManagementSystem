d6d383dbbfb281a321f9e8025f1e18b06f7265d4
BlueLhh <lanhonghui100@gmail.com>
2017-09-10 13:57:21 +0800

    完成订单实体类

--- src/com/lhh/ktv/model/entity/Order.java
+++ src/com/lhh/ktv/model/entity/Order.java
@@ -1,12 +1,110 @@
 package com.lhh.ktv.model.entity;
 
+import java.sql.Date;
 
 /**
  * 
  * 订单实体类
+ * 
  * @author 46512
  *
  */
 public class Order {
+	private Long orderId;// 订单ID
+	private Long ormId;// 包房编号
+	private String orpName;// 消费者姓名
+	private Date ordOpentime;// 开包时间
+	private Date ordEndtime;// 结账时间
+	private double ordrmPrice;// 包房费用
+	private double ordAmtall;// 商品消费总额
+	private double ordAllamtall;// 消费合计
+
+	public Order() {
+
+	}
+
+	public Order(Long orderId, Long ormId, String orpName, Date ordOpentime, Date ordEndtime, double ordrmPrice,
+			double ordAmtall, double ordAllamtall) {
+		super();
+		this.orderId = orderId;
+		this.ormId = ormId;
+		this.orpName = orpName;
+		this.ordOpentime = ordOpentime;
+		this.ordEndtime = ordEndtime;
+		this.ordrmPrice = ordrmPrice;
+		this.ordAmtall = ordAmtall;
+		this.ordAllamtall = ordAllamtall;
+	}
+
+	public Long getOrderId() {
+		return orderId;
+	}
+
+	public void setOrderId(Long orderId) {
+		this.orderId = orderId;
+	}
+
+	public Long getOrmId() {
+		return ormId;
+	}
+
+	public void setOrmId(Long ormId) {
+		this.ormId = ormId;
+	}
+
+	public String getOrpName() {
+		return orpName;
+	}
+
+	public void setOrpName(String orpName) {
+		this.orpName = orpName;
+	}
+
+	public Date getOrdOpentime() {
+		return ordOpentime;
+	}
+
+	public void setOrdOpentime(Date ordOpentime) {
+		this.ordOpentime = ordOpentime;
+	}
+
+	public Date getOrdEndtime() {
+		return ordEndtime;
+	}
+
+	public void setOrdEndtime(Date ordEndtime) {
+		this.ordEndtime = ordEndtime;
+	}
+
+	public double getOrdrmPrice() {
+		return ordrmPrice;
+	}
+
+	public void setOrdrmPrice(double ordrmPrice) {
+		this.ordrmPrice = ordrmPrice;
+	}
+
+	public double getOrdAmtall() {
+		return ordAmtall;
+	}
+
+	public void setOrdAmtall(double ordAmtall) {
+		this.ordAmtall = ordAmtall;
+	}
+
+	public double getOrdAllamtall() {
+		return ordAllamtall;
+	}
+
+	public void setOrdAllamtall(double ordAllamtall) {
+		this.ordAllamtall = ordAllamtall;
+	}
+
+	@Override
+	public String toString() {
+		return "订单信息 [订单编号=" + orderId + ", 包房编号=" + ormId + ", 客户名字=" + orpName + ", 开包时间="
+				+ ordOpentime + ", 结账时间=" + ordEndtime + ", 包房费用=" + ordrmPrice + ", 商品消费总额=" + ordAmtall
+				+ ", 消费合计=" + ordAllamtall + "]";
+	}
 
 }
