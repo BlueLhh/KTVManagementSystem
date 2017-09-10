b67785a62e9dee7cc85f0186d8c1b5cf89a8ee72
BlueLhh <lanhonghui100@gmail.com>
2017-09-10 16:52:40 +0800

    员工服务接口类

---
+++ src/com/lhh/ktv/model/service/IEmployeeService.java
@@ -0,0 +1,19 @@
+package com.lhh.ktv.model.service;
+
+import com.lhh.ktv.exception.ServiceException;
+import com.lhh.ktv.model.entity.Employee;
+
+/**
+ * 
+ * 员工服务类
+ * 实现登录和增加的功能
+ * @author 46512
+ *
+ */
+public interface IEmployeeService {
+	//增加员工功能
+	public void addEmployee(Employee employee) throws ServiceException;
+	//登录功能
+	public void login(String userId, String password) throws ServiceException;
+	//删除功能
+}
