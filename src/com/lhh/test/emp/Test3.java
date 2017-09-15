f67f651d465ab407f784c942f045e0e5ff3461ac
BlueLhh <lanhonghui100@gmail.com>
2017-09-15 19:04:13 +0800

    commit

---
+++ src/com/lhh/test/emp/Test3.java
@@ -0,0 +1,10 @@
+package com.lhh.test.emp;
+
+public class Test3 {
+	public static void main(String[] args) {
+		System.out.println("这是TEST3");
+		System.out.println("这是TEST3第二次上传");
+		System.out.println("测试上传全部还是单个--GITHUB");
+		System.out.println("测试上传--码云");
+	}
+}
