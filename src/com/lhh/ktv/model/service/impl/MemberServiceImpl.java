5e00e9f38d10a6fa43b8d82470331e2c52cf108b
BlueLhh <lanhonghui100@gmail.com>
2017-09-15 20:29:52 +0800

    commit

--- src/com/lhh/ktv/model/service/impl/MemberServiceImpl.java
+++ src/com/lhh/ktv/model/service/impl/MemberServiceImpl.java
@@ -6,6 +6,7 @@ import java.util.List;
 import com.lhh.ktv.common.ConnectionFactory;
 import com.lhh.ktv.common.DBUtils;
 import com.lhh.ktv.common.JdbcTransaction;
+import com.lhh.ktv.exception.DataAccessException;
 import com.lhh.ktv.exception.ServiceException;
 import com.lhh.ktv.model.dao.IMemberDao;
 import com.lhh.ktv.model.dao.impl.MemberDaoImpl;
@@ -39,27 +40,110 @@ public class MemberServiceImpl implements IMemberService{
 		}
 	}
 
+	//删除数据
 	@Override
 	public void delMem(Long id) throws ServiceException {
 		// TODO Auto-generated method stub
-		
+		JdbcTransaction trans = new JdbcTransaction();
+		Connection conn = null;
+		try {
+			// 启动事务
+			conn = ConnectionFactory.getConnection();
+			trans.beginTransaction(conn);
+			memberDao.deletesaveEember(id, conn);
+			// 提交事务
+			trans.commit(conn);
+		} catch (Exception e) {
+			// 回滚事务
+			trans.rollback(conn);
+			throw new ServiceException("删除会员失败！");
+		} finally {
+			DBUtils.close(null, null, conn);
+		}
 	}
 
+	//更新会员的信息
 	@Override
 	public void updateMem(Member member) throws ServiceException {
 		// TODO Auto-generated method stub
-		
+		JdbcTransaction trans = new JdbcTransaction();
+		Connection conn = null;
+
+		try {
+			// 启动事务
+			conn = ConnectionFactory.getConnection();
+			trans.beginTransaction(conn);
+			memberDao.updatesaveEember(member, conn);
+			// 提交事务
+			trans.commit(conn);
+		} catch (DataAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO: handle exception
+			// 回滚事务
+			trans.rollback(conn);
+			//throw new ServiceException("更新员工失败！");
+			e.printStackTrace();
+		} finally {
+			DBUtils.close(null, null, conn);
+		}
 	}
 
+	//按条件查找
 	@Override
 	public Member findMem(Long id) throws ServiceException {
 		// TODO Auto-generated method stub
-		return null;
+		
+		JdbcTransaction trans = new JdbcTransaction();
+		Connection conn = null;
+		Member member = new Member();
+		try {
+			// 启动事务
+			conn = ConnectionFactory.getConnection();
+			trans.beginTransaction(conn);
+			member = memberDao.findsaveEember(id, conn);
+			System.out.println("查找会员成功！");
+			// 提交事务
+			trans.commit(conn);
+		} catch (Exception e) {
+			e.printStackTrace();
+			// 回滚事务
+			trans.rollback(conn);
+			throw new ServiceException("查找会员失败！");
+		} finally {
+			DBUtils.close(null, null, conn);
+		}
+		return member;
 	}
 
+	// 批量查询会员信息
 	@Override
 	public List<Member> findMem(Member member) throws ServiceException {
 		// TODO Auto-generated method stub
+		JdbcTransaction trans = new JdbcTransaction();
+		Connection conn = null;
+		List<Member> list = null;
+		try {
+			// 启动事务
+			conn = ConnectionFactory.getConnection();
+			trans.beginTransaction(conn);
+			list = memberDao.findsaveEember(conn);
+			for (Member mem : list) {
+				System.err.println(mem);
+			}
+			// 提交事务
+			trans.commit(conn);
+		} catch (DataAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// 回滚事务
+			trans.rollback(conn);
+			throw new ServiceException("查询失败失败！");
+		} finally {
+			DBUtils.close(null, null, conn);
+		}
 		return null;
 	}
 
