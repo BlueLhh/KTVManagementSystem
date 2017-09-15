71de8a7e1288d1aa7bbf38813e456165bbc26541
BlueLhh <lanhonghui100@gmail.com>
2017-09-15 19:05:40 +0800

    commit

--- src/com/lhh/ktv/model/service/IMemberService.java
+++ src/com/lhh/ktv/model/service/IMemberService.java
@@ -16,19 +16,19 @@ import com.lhh.ktv.model.entity.Member;
 public interface IMemberService {
 
 	// 增加功能
-	public void addMem(Member Member) throws ServiceException;
+	public void addMem(Member member) throws ServiceException;
 
 	// 删除功能
 	public void delMem(Long id) throws ServiceException;
 
 	// 修改功能
-	public void updateMem(Member Member) throws ServiceException;
+	public void updateMem(Member member) throws ServiceException;
 
 	// 查找功能
 	public Member findMem(Long id) throws ServiceException;
 
 	// 批量查找
-	List<Member> findMem(Member Member) throws ServiceException;
+	List<Member> findMem(Member member) throws ServiceException;
 
 	// 动态查询
 	List<Member> findMem(List<String> conditions);
