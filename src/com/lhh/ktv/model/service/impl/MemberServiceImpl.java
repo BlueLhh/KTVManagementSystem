c5a939d77541a641146a884b03822b0ba3a9c42e
BlueLhh <lanhonghui100@gmail.com>
2017-09-15 19:05:31 +0800

    commit

--- src/com/lhh/ktv/model/service/impl/MemberServiceImpl.java
+++ src/com/lhh/ktv/model/service/impl/MemberServiceImpl.java
@@ -1,17 +1,42 @@
 package com.lhh.ktv.model.service.impl;
 
+import java.sql.Connection;
 import java.util.List;
 
+import com.lhh.ktv.common.ConnectionFactory;
+import com.lhh.ktv.common.DBUtils;
+import com.lhh.ktv.common.JdbcTransaction;
 import com.lhh.ktv.exception.ServiceException;
+import com.lhh.ktv.model.dao.IMemberDao;
+import com.lhh.ktv.model.dao.impl.MemberDaoImpl;
 import com.lhh.ktv.model.entity.Member;
 import com.lhh.ktv.model.service.IMemberService;
 
 public class MemberServiceImpl implements IMemberService{
 
+	private IMemberDao memberDao = new MemberDaoImpl();
+	
+	//添加会员
 	@Override
-	public void addMem(Member Member) throws ServiceException {
+	public void addMem(Member member) throws ServiceException {
 		// TODO Auto-generated method stub
-		
+		JdbcTransaction trans = new JdbcTransaction();
+		Connection conn = null;
+		try {
+			// 启动事务
+			conn = ConnectionFactory.getConnection();
+			trans.beginTransaction(conn);
+			memberDao.saveEember(member, conn);
+			// 提交事务
+			trans.commit(conn);
+		} catch (Exception e) {
+			// 回滚事务
+			trans.rollback(conn);
+//			throw new ServiceException("添加员工失败！");
+			e.printStackTrace();
+		} finally {
+			DBUtils.close(null, null, conn);
+		}
 	}
 
 	@Override
@@ -21,7 +46,7 @@ public class MemberServiceImpl implements IMemberService{
 	}
 
 	@Override
-	public void updateMem(Member Member) throws ServiceException {
+	public void updateMem(Member member) throws ServiceException {
 		// TODO Auto-generated method stub
 		
 	}
@@ -33,7 +58,7 @@ public class MemberServiceImpl implements IMemberService{
 	}
 
 	@Override
-	public List<Member> findMem(Member Member) throws ServiceException {
+	public List<Member> findMem(Member member) throws ServiceException {
 		// TODO Auto-generated method stub
 		return null;
 	}
