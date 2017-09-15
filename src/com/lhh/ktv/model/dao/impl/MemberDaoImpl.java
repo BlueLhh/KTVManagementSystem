7551e3c15bc270913049a706b7ea09fd6a3fa732
BlueLhh <lanhonghui100@gmail.com>
2017-09-15 20:29:57 +0800

    commit

--- src/com/lhh/ktv/model/dao/impl/MemberDaoImpl.java
+++ src/com/lhh/ktv/model/dao/impl/MemberDaoImpl.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.lhh.ktv.common.JdbcTemplate;
@@ -11,6 +12,7 @@ import com.lhh.ktv.common.PreparedStatementSetter;
 import com.lhh.ktv.common.RowCallBackHandler;
 import com.lhh.ktv.exception.DataAccessException;
 import com.lhh.ktv.model.dao.IMemberDao;
+import com.lhh.ktv.model.entity.Employee;
 import com.lhh.ktv.model.entity.Member;
 
 public class MemberDaoImpl implements IMemberDao {
@@ -18,7 +20,7 @@ public class MemberDaoImpl implements IMemberDao {
 	// 全局变量id
 	Long id = 0L;
 
-	//添加会员
+	// 添加会员
 	@Override
 	public void saveEember(Member member, Connection conn) throws DataAccessException {
 		// TODO Auto-generated method stub
@@ -47,30 +49,91 @@ public class MemberDaoImpl implements IMemberDao {
 		});
 	}
 
+	// 删除数据
 	@Override
 	public void deletesaveEember(Long id, Connection conn) throws DataAccessException {
 		// TODO Auto-generated method stub
-
+		System.out.println("deletesaveEember id=" + id);
+		JdbcTemplate jt = new JdbcTemplate(conn);
+		String sql = "delete from k_mem where mem_id = ?";
+		jt.update(sql, new PreparedStatementSetter() {
+			public void setValues(PreparedStatement pstmt) throws SQLException {
+				pstmt.setLong(1, id);
+			}
+		});
 	}
 
+	// 更改会员信息
 	@Override
 	public void updatesaveEember(Member member, Connection conn) throws DataAccessException {
 		// TODO Auto-generated method stub
 
+		JdbcTemplate jt = new JdbcTemplate(conn);
+		String sql = "update k_mem " + "set mem_name = ?,mem_gender = ?,mem_age = ?,mem_phone = ? "
+				+ "where mem_id = ?";
+
+		jt.update(sql, new PreparedStatementSetter() {
+			public void setValues(PreparedStatement pstmt) throws SQLException {
+				pstmt.setString(1, member.getMemName());
+				pstmt.setString(2, member.getMemGender());
+				pstmt.setByte(3, member.getMemAge());
+				pstmt.setString(4, member.getMemPhone());
+				pstmt.setLong(5, member.getMemId());
+			}
+		});
 	}
 
+	// 按ID查找会员
 	@Override
 	public Member findsaveEember(Long id, Connection conn) throws DataAccessException {
 		// TODO Auto-generated method stub
-		return null;
+
+		JdbcTemplate jt = new JdbcTemplate(conn);
+		final Member member = new Member();
+		String sql = "select mem_id,mem_name,mem_gender,mem_age,mem_phone from k_mem where mem_id = ?";
+		jt.query(sql, new PreparedStatementSetter() {
+			public void setValues(PreparedStatement pstmt) throws SQLException {
+				pstmt.setLong(1, id);
+			}
+		}, new RowCallBackHandler() {
+			public void processRow(ResultSet rs) throws SQLException {
+				if (rs.next()) {
+					member.setMemId(rs.getLong(1));
+					member.setMemName(rs.getString(2));
+					member.setMemGender(rs.getString(3));
+					member.setMemAge(rs.getByte(4));
+					member.setMemPhone(rs.getString(5));
+				}
+			}
+		});
+
+		return member;
 	}
 
+	// 批量查询会员
 	@Override
 	public List<Member> findsaveEember(Connection conn) throws DataAccessException {
 		// TODO Auto-generated method stub
-		return null;
+		JdbcTemplate jt = new JdbcTemplate(conn);
+		final List<Member> list = new ArrayList<Member>();
+		String sql = "select mem_id,mem_name,mem_gender,mem_age,mem_phone from k_mem";
+		jt.query(sql, new RowCallBackHandler() {
+			public void processRow(ResultSet rs) throws SQLException {
+				while (rs.next()) {
+					Member member = new Member();
+					member.setMemId(rs.getLong(1));
+					member.setMemName(rs.getString(2));
+					member.setMemGender(rs.getString(3));
+					member.setMemAge(rs.getByte(4));
+					member.setMemPhone(rs.getString(5));
+					list.add(member);
+				}
+			}
+		});
+		return list;
 	}
 
+	// 动态查询会员
 	@Override
 	public List<Member> findsaveEember(List<String> conditions, Connection conn) throws DataAccessException {
 		// TODO Auto-generated method stub
