386bfe56a233071d8cabe6e3cbccfc87478ea0a9
BlueLhh <lanhonghui100@gmail.com>
2017-09-15 20:29:34 +0800

    commit

---
+++ src/com/lhh/test/mem/FindOneMemTest.java
@@ -0,0 +1,20 @@
+package com.lhh.test.mem;
+
+import com.lhh.ktv.exception.ServiceException;
+import com.lhh.ktv.model.entity.Member;
+import com.lhh.ktv.model.service.impl.MemberServiceImpl;
+
+public class FindOneMemTest {
+	public static void main(String[] args) {
+		MemberServiceImpl mem = new MemberServiceImpl();
+		Member member = new Member();
+		try {
+			member = mem.findMem(2L);
+			System.out.println(member);
+
+		} catch (ServiceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
