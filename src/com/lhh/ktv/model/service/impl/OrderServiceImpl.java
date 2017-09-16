3d44f6639f3706f3cd103f5ebcac0785eb34d71f
BlueLhh <lanhonghui100@gmail.com>
2017-09-16 16:35:40 +0800

    commit

--- src/com/lhh/ktv/model/service/impl/OrderServiceImpl.java
+++ src/com/lhh/ktv/model/service/impl/OrderServiceImpl.java
@@ -1,47 +1,184 @@
 package com.lhh.ktv.model.service.impl;
 
+import java.sql.Connection;
 import java.util.List;
 
+import com.lhh.ktv.common.ConnectionFactory;
+import com.lhh.ktv.common.DBUtils;
+import com.lhh.ktv.common.JdbcTransaction;
+import com.lhh.ktv.exception.DataAccessException;
 import com.lhh.ktv.exception.ServiceException;
+import com.lhh.ktv.model.dao.IOrderDao;
+import com.lhh.ktv.model.dao.impl.OrderDaoImpl;
 import com.lhh.ktv.model.entity.Order;
 import com.lhh.ktv.model.service.IOrderService;
 
-public class OrderServiceImpl implements IOrderService{
+public class OrderServiceImpl implements IOrderService {
 
+	private IOrderDao orderDao = new OrderDaoImpl();
+	
+	// 增
 	@Override
 	public void addOrder(Order order) throws ServiceException {
 		// TODO Auto-generated method stub
-		
+		JdbcTransaction trans = new JdbcTransaction();
+		Connection conn = null;
+		try {
+			// 启动事务
+			conn = ConnectionFactory.getConnection();
+			trans.beginTransaction(conn);
+			orderDao.saveOrder(order, conn);
+			// 提交事务
+			trans.commit(conn);
+		} catch (Exception e) {
+			// 回滚事务
+			trans.rollback(conn);
+			// throw new ServiceException("添加员工失败！");
+			e.printStackTrace();
+		} finally {
+			DBUtils.close(null, null, conn);
+		}
 	}
 
+	// 删
 	@Override
 	public void delOrder(Long id) throws ServiceException {
 		// TODO Auto-generated method stub
-		
+		JdbcTransaction trans = new JdbcTransaction();
+		Connection conn = null;
+		try {
+			// 启动事务
+			conn = ConnectionFactory.getConnection();
+			trans.beginTransaction(conn);
+			orderDao.deleteOrder(id, conn);
+			// 提交事务
+			trans.commit(conn);
+		} catch (Exception e) {
+			// 回滚事务
+			trans.rollback(conn);
+			throw new ServiceException("删除失败！");
+		} finally {
+			DBUtils.close(null, null, conn);
+		}
 	}
 
+	// 改
 	@Override
 	public void updateOrder(Order order) throws ServiceException {
 		// TODO Auto-generated method stub
-		
+		JdbcTransaction trans = new JdbcTransaction();
+		Connection conn = null;
+
+		try {
+			// 启动事务
+			conn = ConnectionFactory.getConnection();
+			trans.beginTransaction(conn);
+			orderDao.updatOrder(order, conn);			
+			// 提交事务
+			trans.commit(conn);
+		} catch (DataAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO: handle exception
+			// 回滚事务
+			trans.rollback(conn);
+			//throw new ServiceException("更新员工失败！");
+			e.printStackTrace();
+		} finally {
+			DBUtils.close(null, null, conn);
+		}
 	}
 
+	// 查ID
 	@Override
 	public Order findOrder(Long id) throws ServiceException {
 		// TODO Auto-generated method stub
-		return null;
+		JdbcTransaction trans = new JdbcTransaction();
+		Connection conn = null;
+		Order order = new Order();
+		try {
+			// 启动事务
+			conn = ConnectionFactory.getConnection();
+			trans.beginTransaction(conn);
+			order = orderDao.findOrder(id, conn);
+			System.out.println("查找成功！");
+			// 提交事务
+			trans.commit(conn);
+		} catch (Exception e) {
+			e.printStackTrace();
+			// 回滚事务
+			trans.rollback(conn);
+			throw new ServiceException("查找失败！");
+		} finally {
+			DBUtils.close(null, null, conn);
+		}
+		return order;
 	}
 
+	// 批量查询
 	@Override
 	public List<Order> findOrder(Order order) throws ServiceException {
 		// TODO Auto-generated method stub
-		return null;
+		JdbcTransaction trans = new JdbcTransaction();
+		Connection conn = null;
+		List<Order> list = null;
+		try {
+			// 启动事务
+			conn = ConnectionFactory.getConnection();
+			trans.beginTransaction(conn);
+			list = orderDao.findOrder(conn);
+			for (Order ord : list) {
+				System.out.println(ord);
+			}
+			// 提交事务
+			trans.commit(conn);
+		} catch (DataAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// 回滚事务
+			trans.rollback(conn);
+			throw new ServiceException("查询失败失败！");
+		} finally {
+			DBUtils.close(null, null, conn);
+		}
+		return list;
 	}
 
+	// 动态查询
 	@Override
 	public List<Order> findOrder(List<String> conditions) {
 		// TODO Auto-generated method stub
-		return null;
+		
+		JdbcTransaction trans = new JdbcTransaction();
+		Connection conn = null;
+		List<Order> list = null;
+		try {
+			// 启动事务
+			conn = ConnectionFactory.getConnection();
+			trans.beginTransaction(conn);
+			list = orderDao.findOrder(conditions, conn);
+			System.out.println("动态查询成功！");
+			// 提交事务
+			trans.commit(conn);
+		} catch (DataAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// 回滚事务
+			trans.rollback(conn);
+			try {
+				throw new ServiceException("动态查询失败失败！");
+			} catch (ServiceException ee) {
+				// TODO Auto-generated catch block
+				ee.printStackTrace();
+			}
+		} finally {
+			DBUtils.close(null, null, conn);
+		}
+		
+		return list;
 	}
 
 }
