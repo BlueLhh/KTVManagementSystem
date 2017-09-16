7e70a56efec26152133046308fabd42b64c6951d
BlueLhh <lanhonghui100@gmail.com>
2017-09-16 16:35:36 +0800

    commit

--- src/com/lhh/ktv/model/entity/Reserve.java
+++ src/com/lhh/ktv/model/entity/Reserve.java
@@ -1,6 +1,5 @@
 package com.lhh.ktv.model.entity;
 
-import java.sql.Date;
 
 /**
  * 预定信息表实体类
@@ -10,29 +9,31 @@ import java.sql.Date;
  */
 public class Reserve {
 	private Long resId;// 预定编号
-	private Room roomId;// 预定包房编号（外键）
-	private Date resTime;// 预定时间
-	private Date resendTime;// 保留时间
+	private Room room = new Room();// 预定包房编号（外键）
+	private String resTime;// 预定时间
+	private String resendTime;// 保留时间
 	private String resPname;// 预定人姓名
-	private Long resmemId;// 会员编号
+	private Long resmemId;// 会员编号(0为非会员)
 	private String resPhone;// 预定人号码
 
+	
+	
 	public Reserve() {
 
 	}
 
-	public Reserve(Long resId, Room roomId, Date resTime, Date resendTime, String resPname, Long resmemId,
+	public Reserve(Long resId, Room room, String resTime, String resendTime, String resPname, Long resmemId,
 			String resPhone) {
 		super();
 		this.resId = resId;
-		this.roomId = roomId;
+		this.room = room;
 		this.resTime = resTime;
 		this.resendTime = resendTime;
 		this.resPname = resPname;
 		this.resmemId = resmemId;
 		this.resPhone = resPhone;
 	}
-
+	
 	public Long getResId() {
 		return resId;
 	}
@@ -41,27 +42,27 @@ public class Reserve {
 		this.resId = resId;
 	}
 
-	public Room getRoomId() {
-		return roomId;
+	public Room getRoom() {
+		return room;
 	}
 
-	public void setRoomId(Room roomId) {
-		this.roomId = roomId;
+	public void setRoom(Room room) {
+		this.room = room;
 	}
 
-	public Date getResTime() {
+	public String getResTime() {
 		return resTime;
 	}
 
-	public void setResTime(Date resTime) {
+	public void setResTime(String resTime) {
 		this.resTime = resTime;
 	}
 
-	public Date getResendTime() {
+	public String getResendTime() {
 		return resendTime;
 	}
 
-	public void setResendTime(Date resendTime) {
+	public void setResendTime(String resendTime) {
 		this.resendTime = resendTime;
 	}
 
@@ -91,7 +92,7 @@ public class Reserve {
 
 	@Override
 	public String toString() {
-		return "预定信息 [预定编号=" + resId + ", 包房编号=" + roomId + ", 预定时间=" + resTime + ", 保留时间="
+		return "预定信息 [预定编号=" + resId + ", 包房编号=" + room + ", 预定时间=" + resTime + ", 保留时间="
 				+ resendTime + ", 预订人姓名=" + resPname + ", 会员编号=" + resmemId + ", 预订人号码=" + resPhone + "]";
 	}
 
