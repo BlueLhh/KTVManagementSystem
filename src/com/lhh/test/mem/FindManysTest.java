106d2a50a845d21978221d2b52f7a3285924dbdb
BlueLhh <lanhonghui100@gmail.com>
2017-09-17 12:47:08 +0800

    commit

--- src/com/lhh/test/mem/FindManysTest.java
+++ src/com/lhh/test/mem/FindManysTest.java
@@ -10,17 +10,17 @@ import com.lhh.ktv.model.service.impl.MemberServiceImpl;
 public class FindManysTest {
 	public static void main(String[] args) {
 		String name;
-		String phone;
+		//String phone;
 		@SuppressWarnings("resource")
 		Scanner sc = new Scanner(System.in);
 		System.out.println("查询的姓名："); 
 		name = sc.next();
-		System.out.println("查询的手机号码：");
-		phone = sc.next();
+		//System.out.println("查询的手机号码：");
+		//phone = sc.next();
 		MemberServiceImpl mem = new MemberServiceImpl();
 		List<String> conditions = new ArrayList<String>();
 		conditions.add("mem_name like '%" + name + "%'");
-		conditions.add("mem_phone like '%" + phone + "%'");
+		//conditions.add("mem_phone like '%" + phone + "%'");
 		List<Member> list = mem.findMem(conditions);
 		System.out.println(list);
 	}
