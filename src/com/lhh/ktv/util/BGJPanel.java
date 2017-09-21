8ae5e19cfaac2611876d2f0e47f809b69590a53f
BlueLhh <lanhonghui100@gmail.com>
2017-09-21 10:26:50 +0800

    commit

--- src/com/lhh/ktv/util/BGJPanel.java
+++ src/com/lhh/ktv/util/BGJPanel.java
@@ -13,62 +13,72 @@ import javax.swing.JPanel;
  * @author 46512
  *
  */
-public class BGJPanel extends JPanel{
+public class BGJPanel extends JPanel {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 5939914134096358498L;
-	
+
 	ImageIcon icon;
 	Image img;
-	
-	public void goodsInfopanelBG(){
-		icon = new ImageIcon(getClass().getResource("/images/infopanelbg.jpg"));//在src文件下
-		img = icon.getImage();
-	}
-	
-	public void infofirstpanelBG(){
-		icon = new ImageIcon(getClass().getResource("/images/infofirstpanel.jpg"));//在src文件下
-		img = icon.getImage();
-	}
-	
-	public void topPanel(){
-		icon = new ImageIcon(getClass().getResource("/images/topbg.jpg"));//在src文件下
-		img = icon.getImage();
-	}
-	
-	public void loginPanel(){
-		icon = new ImageIcon(getClass().getResource("/images/login.png"));//在src文件下
+
+	public void busOrderBG() {
+		icon = new ImageIcon(getClass().getResource("/images/ordertopbg.jpg"));// 在src文件下
 		img = icon.getImage();
 	}
 
-	public void mainPanel(){
+	public void busJpanelBG() {
+		icon = new ImageIcon(getClass().getResource("/images/busmainbg.jpg"));// 在src文件下
+		img = icon.getImage();
+	}
+
+	public void goodsInfopanelBG() {
+		icon = new ImageIcon(getClass().getResource("/images/infopanelbg.jpg"));// 在src文件下
+		img = icon.getImage();
+	}
+
+	public void infofirstpanelBG() {
+		icon = new ImageIcon(getClass().getResource("/images/infofirstpanel.jpg"));// 在src文件下
+		img = icon.getImage();
+	}
+
+	public void topPanel() {
+		icon = new ImageIcon(getClass().getResource("/images/topbg.jpg"));// 在src文件下
+		img = icon.getImage();
+	}
+
+	public void loginPanel() {
+		icon = new ImageIcon(getClass().getResource("/images/login.png"));// 在src文件下
+		img = icon.getImage();
+	}
+
+	public void mainPanel() {
 		icon = new ImageIcon(getClass().getResource("/images/BGmain.jpg"));
 		img = icon.getImage();
 	}
-	
-	public void paintComponent(Graphics g){
+
+	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		g.drawImage(img, 0, 0, this.getWidth(),this.getHeight(),this);
+		g.drawImage(img, 0, 0, this.getWidth(), this.getHeight(), this);
 	}
-	
-	public void bigMainFrame(){
+
+	public void bigMainFrame() {
 		icon = new ImageIcon(getClass().getResource("/images/mainFrame.jpg"));
 		img = icon.getImage();
 	}
-	
-	public void menBg(){
+
+	public void menBg() {
 		icon = new ImageIcon(getClass().getResource("/images/menbg.jpg"));
 		img = icon.getImage();
 	}
-	
-	public void fristBg(){
+
+	public void fristBg() {
 		icon = new ImageIcon(getClass().getResource("/images/frist.jpg"));
 		img = icon.getImage();
 	}
-	
-	public void empDateBg(){
+
+	public void empDateBg() {
 		icon = new ImageIcon(getClass().getResource("/images/dataemp.jpg"));
 		img = icon.getImage();
 	}
